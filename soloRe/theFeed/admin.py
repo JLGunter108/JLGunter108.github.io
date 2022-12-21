d366584c7fb8017a3e02d211be7b495574bbdc64
John Gunter <JLGunter108@gmail.com>
2022-12-21 11:26:39 -0800

    CS TEST

--- soloRe/theFeed/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post, Album, Profile, Comment
-
-admin.site.register(Post)
-admin.site.register(Album)
-admin.site.register(Profile)
-admin.site.register(Comment)
-
--- soloRe/theFeed/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThefeedConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'theFeed'
