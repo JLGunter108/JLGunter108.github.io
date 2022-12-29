abc09f5950b80e4b4be7c91035603580be51acc7
John Gunter <99700729+JLGunter108@users.noreply.github.com>
2022-12-29 12:25:29 -0800

    class tweak

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var select = Document.querySelectorAll('.currency'),
+var select = Document.querySelectorAll('#currency'),
 input_currency = Document.getElementByID('input_currency'),
 output_currency = Document.getElementByID('output_currency');
 
