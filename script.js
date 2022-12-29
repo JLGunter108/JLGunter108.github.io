fbffe4cb82a57e7ecd35689bb2f5cb25411cb281
John Gunter <99700729+JLGunter108@users.noreply.github.com>
2022-12-29 12:22:27 -0800

    loop fix

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ fetch(`https://${host}/currencies`)
     .then((data) => data.json())
     .then((data) => {
         const entries = Object.entries(data);
-        entries.forEach(element => {
-            select[0].innerHTML += `<option value="${element}">${element}</option>`;
-            select[1].innerHTML += `<option value="${element}">${element}</option>`;
-        });
+        for(i=0; i<entries.length; i++) {
+            select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+            select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+        }
     });
+
 function converter() {
     fetch(`https://${host}/latest?amount=${input_currency.value}&from=${select[0].value}&to=${select[1].value}`)
         .then(val => val.json())
